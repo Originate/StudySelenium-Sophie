10432343d04d5206246578241cf7d30dcc39b88a
Sophie Shen <sophie.shen@originate.com>
2019-03-13 15:09:53 -0400

    test 2: resilient test

---
+++ sgb-test-1-2/pages/dynamic_loading_page.py
@@ -0,0 +1,30 @@
+# filename: pages/dynamic_loading_page.py
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+class DynamicLoadingPage():
+    _start_button = {"by": By.CSS_SELECTOR, "value": "#start button"}
+    _finish_text = {"by": By.ID, "value": "finish"}
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def load_example(self, example_number):
+        self.driver.get("http://the-internet.herokuapp.com/dynamic_loading/" + example_number)
+        self.driver.find_element(self._start_button["by"], self._start_button["value"]).click()
+
+    def finish_text_present(self):
+        return self._wait_for_is_displayed(self._finish_text, 10)
+
+    def _wait_for_is_displayed(self, locator, timeout):
+        try:
+            wait = WebDriverWait(self.driver, timeout)
+            wait.until(
+                expected_conditions.visibility_of_element_located((locator['by'], locator['value']))
+            )
+        except TimeoutException:
+            return False
+
+        return True
